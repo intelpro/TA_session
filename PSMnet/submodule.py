c28feb76f43605f625980267c6722a9e7af7c6ed
Taewoo kim <intelpro@kaist.ac.kr>
2020-07-30 19:21:22 +0900

    Update submodule.py

--- PSMnet/submodule.py
+++ PSMnet/submodule.py
@@ -150,9 +150,6 @@ class disparityregression(nn.Module):
         out = torch.sum(x*disp,1)
         return out
 
-def convbn(in_planes, out_planes, kernel_size, stride, pad, dilation):
-    return nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=dilation if dilation > 1 else pad, dilation = dilation, bias=False), nn.BatchNorm2d(out_planes))
-
 def convbn_3d(in_planes, out_planes, kernel_size, stride, pad):
 
     return nn.Sequential(nn.Conv3d(in_planes, out_planes, kernel_size=kernel_size, padding=pad, stride=stride,bias=False),
